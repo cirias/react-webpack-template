2ad927e57e19a737e0ab046d9fb223e9001c119a
Sirius <fhc023@gmail.com>
2015-12-21 10:42:45 +0800

    initial commit

---
+++ app/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+import Hello from './components/hello.jsx';
+
+main();
+
+function main() {
+  ReactDOM.render(<Hello />, document.getElementById('app'));
+}
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var config = {
+  entry: [
+    './app/main.js'
+  ],
+  output: {
+      path: './build',
+      filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/, // A regexp to test the require path. accepts either js or jsx
+      loader: 'babel', // The module to load. "babel" is short for "babel-loader"
+      query: {
+	      presets:['es2015','react']
+      }
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.(png|jpg)$/,
+      loader: 'url?limit=25000'
+    }],
+  }
+};
+
+module.exports = config;
